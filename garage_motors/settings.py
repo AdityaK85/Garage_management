7bb7ff4c78b9037f7caab3922c053679e41bb7ab
Aditya85 <adityakothekar79@gmail.com>
2023-05-20 08:07:39 +0530

    Add Ip address replace Astriek

--- garage_motors/settings.py
+++ garage_motors/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-sp!-lr$!0n0!jhfy(5n7f6avm)%1bpp150w#iu6k_9(kl0%o!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]  ---------------------------------------------------------> "*" this astrick allowed all port
+ALLOWED_HOSTS = ["16.16.200.45"] #----------------------> this is give security to enter your unique ip address 
 
 
 # Application definition
