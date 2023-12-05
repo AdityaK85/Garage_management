1373e1ee617d5de89be644b38c14e9024830155a
aditya <adityakothekar79@gmail.com>
2023-12-06 00:07:03 +0530

    today

--- garage_motors/settings.py
+++ garage_motors/settings.py
@@ -130,7 +130,7 @@ EMAIL_HOST_PASSWORD = 'tapkpadlebfvurdn'
 
 STATIC_URL = 'static/'
 STATICDIRS = (os.path.join(BASE_DIR,'/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
