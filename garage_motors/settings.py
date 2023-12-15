385bbbcb2799e2b0a903fb56d37a676448015a03
aditya <adityakothekar79@gmail.com>
2023-12-16 00:03:28 +0530

    dl

--- garage_motors/settings.py
+++ garage_motors/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sp!-lr$!0n0!jhfy(5n7f6avm)%1bpp150w#iu6k_9(kl0%o!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*'] 
 # ALLOWED_HOSTS = ['.vercel.app', '.now.sh'] 
 
